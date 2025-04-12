c41d53a2f3ee0587e2e5ce965c72b369100db55e
mohdamangithub <mansooriaman025@gmail.com>
2025-04-12 18:22:34 +0530

    complete

--- BackEnd/src/app.js
+++ BackEnd/src/app.js
@@ -5,16 +5,17 @@ const cors = require('cors')
 const app = express()
 
 //Configure Cors
-const corsOptions = {
-    origin: process.env.FRONTEND_URL,
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type", "Authorization"]
-};
+// const corsOptions = {
+//     origin: process.env.FRONTEND_URL,
+//     methods: ["GET", "POST", "PUT", "DELETE"],
+//     allowedHeaders: ["Content-Type", "Authorization"]
+// };
 
-app.use(cors(corsOptions))
+app.use(cors())
 
 
 app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
 app.get('/', (req, res) => {
     res.send('Hello World')
